a44938f4d112e8697c406c4a6dba3ea45ffa8cbf
Kenneth DAquila <kfd182@gmail.com>
2021-09-09 10:38:23 -0500

    Add project files.

---
+++ WoodworkingShop.UnitTests/Entities/ProductTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoodworkingShop.Domain;
+using Xunit;
+
+namespace WoodworkingShop.UnitTests
+{
+    public class ProductTest
+    {
+        [Theory]
+        [InlineData("Mitre Saw")]
+        public void UpdateNameWithValidData(string name)
+        {
+            Product product = new Product(new Guid(), name: "Table Saw", description: "Jobsite table saw", price: 350.0m);
+
+            product.Name = name;
+
+            Assert.Equal(name, product.Name);
+        }
+    }
+
+
+}
