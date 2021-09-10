d5851b3ffd1c47ef88b54b7e24e52baaabf2629b
Kenneth DAquila <kfd182@gmail.com>
2021-09-10 10:16:55 -0500

    Add Product Integration Test

---
+++ WoodworkingShop.IntegrationTests/IntegrationTestFixture.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoodworkingShop.Domain;
+using WoodworkingShop.Infrastructure;
+
+namespace WoodworkingShop.IntegrationTests
+{
+    public class IntegrationTestFixture
+    {
+        public static async Task<AppDbContext> CreateDbContext()
+        {
+            var dbOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+            var appDbContext = new AppDbContext(dbOptions);
+
+            await appDbContext.Products.AddAsync(new Product(new Guid(), "Band Saw", "Benchtop band saw", 400.0m));
+            await appDbContext.SaveChangesAsync();
+
+            return appDbContext;
+        }
+    }
+}
---
+++ WoodworkingShop.IntegrationTests/ReadProductTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using WoodworkingShop.Domain;
+using WoodworkingShop.Infrastructure;
+using Xunit;
+
+namespace WoodworkingShop.IntegrationTests
+{
+    public class ReadProductTest
+    {
+        [Fact]
+        public async Task ReadProduct()
+        {
+            AppDbContext appDbContext = await IntegrationTestFixture.CreateDbContext();
+
+            Product product = appDbContext.Products.FirstOrDefault(p => p.Name == "Band Saw");
+
+            Assert.NotNull(product);
+        }
+
+
+    }
+}
