06be0fc4370bb1979dbc90fbef1ec37d01445786
Kenneth DAquila <kfd182@gmail.com>
2021-09-16 14:26:54 -0500

    Simplify base class referencee

--- WoodworkingShop.IntegrationTests/Products/GetProductById.cs
+++ WoodworkingShop.IntegrationTests/Products/GetProductById.cs
@@ -11,13 +11,13 @@ namespace WoodworkingShop.IntegrationTests.Products
         [Fact]
         public override async Task TestAsync()
         {
-            await base._dbContext.Products.AddAsync(new Product(
+            await _dbContext.Products.AddAsync(new Product(
                 id: new Guid("a2d23a0e-aaa7-4d66-9c9e-08d974730ded"),
                 name: "Band Saw",
                 description: "Benchtop band saw",
                 price: 400.0m));
-            await base._dbContext.SaveChangesAsync();
-            IRepository<Product> productRepo = new AppRepository<Product>(base._dbContext, new QueryOptionsEvaluator<Product>());
+            await _dbContext.SaveChangesAsync();
+            IRepository<Product> productRepo = new AppRepository<Product>(_dbContext, new QueryOptionsEvaluator<Product>());
 
             Product product = await productRepo.GetByIdAsync(new Guid("a2d23a0e-aaa7-4d66-9c9e-08d974730ded"));
 
