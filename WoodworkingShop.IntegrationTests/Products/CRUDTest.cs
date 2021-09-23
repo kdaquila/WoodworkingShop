ae98a033ac5982619d76e2c5be6fc67d7edf4608
Kenneth DAquila <kfd182@gmail.com>
2021-09-23 17:54:41 -0500

    Refactored integration tests

---
+++ WoodworkingShop.IntegrationTests/Products/CRUDTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using WoodworkingShop.Domain;
+using WoodworkingShop.Infrastructure;
+using Xunit;
+
+namespace WoodworkingShop.IntegrationTests.Products
+{
+    public class CRUDTest
+    {
+        [Fact]
+        public async Task TestAsync()
+        {
+            using (var context = TestDbContextProvider.createNewContext())
+            {
+                // Setup
+                IRepository<Product> productRepo = new AppRepository<Product>(context, new QueryOptionsEvaluator<Product>());
+                Guid productId = new Guid("a2d23a0e-aaa7-4d66-9c9e-08d974730ded");
+                string originalName = "Band Saw";
+                string modifiedName = "Band Saw2";
+                Product product = new Product(
+                    id: productId,
+                    name: originalName,
+                    description: "Benchtop band saw",
+                    price: 400.0m);
+
+                // Create
+                await productRepo.AddAsync(product);
+                Product product1 = await productRepo.GetByIdAsync(productId);
+                Assert.Equal(originalName, product1.Name);
+
+                // Update
+                product1.Name = modifiedName;
+                await productRepo.UpdateAsync(product1);
+                Product product2 = await productRepo.GetByIdAsync(productId);
+                Assert.Equal(modifiedName, product2.Name);
+
+                // Delete
+                await productRepo.DeleteAsync(product2);
+                await Assert.ThrowsAsync<DbObjectNotFound>(() => productRepo.GetByIdAsync(productId));
+            }
+        }
+    }
+}
---
+++ WoodworkingShop.IntegrationTests/TestDbContextProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoodworkingShop.Domain;
+using WoodworkingShop.Infrastructure;
+
+namespace WoodworkingShop.IntegrationTests
+{
+    public class TestDbContextProvider
+    {
+        public static AppDbContext createNewContext()
+        {
+            IConfiguration configuration = GetConfiguration();
+            string connStr = configuration.GetConnectionString("WoodworkingShopTesting");
+
+            AppDbContext context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer(connStr)
+                .Options); ;
+
+            context.Database.BeginTransaction();
+
+            return context;
+        }
+
+        public static IConfiguration GetConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .Build();
+            return config;
+        }
+    }
+}
